6a591e2ce6d97f245f7f7ae9543a96788217c97f
UsamaSadiq <usama.sadiq@arbisoft.com>
2022-02-03 15:32:12 +0500

    build: Run tests

--- codejail_test.py
+++
@@ -1,4 +0,0 @@
-import codejail.jail_code
-codejail.jail_code.configure('python', 'codejailvenv/bin/python')
-import codejail.safe_exec
-codejail.safe_exec.safe_exec("import os\nos.system('ls /etc')", {})
